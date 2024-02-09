a19d2cfb319ccb38f4d8c2744986a4e3cf450c48
Monica Harabagiu <143797737+monica-harabagiu@users.noreply.github.com>
2024-02-09 15:15:44 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style lang="scss">
+
+@use './style/general.scss' as *;
+
+</style>
