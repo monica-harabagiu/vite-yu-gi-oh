8970951707e2b46b5f21eed6decddc577b59dc6d
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-02-09 16:08:12 +0100

    header component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppHeader from './components/AppHeader.vue'
+import { store } from './store'
+
+export default {
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+      store
+    }
+  }
+}
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader/>
 </template>
 
 <style lang="scss">
-
 @use './style/general.scss' as *;
-
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,45 @@
+<script>
+
+export default {
+  name: "AppHeader"
+}
+
+</script>
+
+<template>
+  <header>
+
+    <div>
+
+      <figure>
+        <img src="../assets/img/Yu-Gi-Oh!.png" alt="Yu-Gi-Oh Logo">
+      </figure>
+      <h1>Yu-Gi-Oh Api</h1>
+
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+header {
+
+  background-color: white;
+  padding-block: 30px;
+
+  div {
+
+    display: flex;
+    align-items: center;
+    width: 80%;
+    margin: 0 auto;
+
+    figure {
+
+      width: 100px;
+    }
+  }
+
+
+}
+</style>
